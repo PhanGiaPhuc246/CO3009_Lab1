fb2247487ba78150d1066fcc1db40d8592bbde60
DESKTOP-EDHDNPF\Admin <phucphan47@gmail.com>
2024-09-23 10:32:40 +0700

    Init for Ex1

--- Lab1/Core/Src/main.c
+++ Lab1/Core/Src/main.c
@@ -205,10 +205,10 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LED_RED0_Pin|LED_YELLOW0_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, LED_RED0_Pin|LED_YELLOW0_Pin|LED_GREEN0_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : LED_RED0_Pin LED_YELLOW0_Pin */
-  GPIO_InitStruct.Pin = LED_RED0_Pin|LED_YELLOW0_Pin;
+  /*Configure GPIO pins : LED_RED0_Pin LED_YELLOW0_Pin LED_GREEN0_Pin */
+  GPIO_InitStruct.Pin = LED_RED0_Pin|LED_YELLOW0_Pin|LED_GREEN0_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
